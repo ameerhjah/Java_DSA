b2ec276811b4438530920738cd14418c2f3c6c70
mdameerhumjah <mdameerhmjah@gmail.com>
2023-01-11 02:25:02 +0530

    NumberExample.java added

---
+++ Recursion/NumberExample.java
@@ -0,0 +1,20 @@
+package Recursion;
+import java.util.*;
+
+public class NumberExample {
+    public static void main(String[] args) {
+        //printing the number from 1 to n using recusion 
+
+        //calling the function 
+        printNumber(1);
+
+    }
+
+static void printNumber(int n ){
+        if(n > 5){
+            return ;
+        }
+        System.out.print(n+" ");
+        printNumber(n+1);
+    }
+}
